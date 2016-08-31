38ad3c9d496fb038e6adb75954af020df929de1a
OmegaNalphA <agg_anshul@yahoo.com>
2016-08-31 00:45:30 -0700

    Mailchimp Skeleton
    Backend skeleton to access the Mailchimp API. Currently supports export
    and read api's.

--- server.js
+++ server.js
@@ -8,6 +8,51 @@ const config = require('./config')
 
 const app = new express()
 const routes = require('./api/routes')
+
+var MailChimpAPI = require('mailchimp').MailChimpAPI;
+ 
+var apiKey = '0b1111389e3381feeb3ad8c9f197d745-us7';
+ 
+try {
+    var api = new MailChimpAPI(apiKey, { version : '2.0' });
+} catch (error) {
+    console.log(error.message);
+}
+
+api.call('campaigns', 'list', { start: 0, limit: 25 }, function (error, data) {
+    if (error)
+        console.log(error.message);
+    else
+        //console.log(JSON.stringify(data));
+        console.log("successful access of lists"); 
+});
+
+
+//whats our campaign id? check the sandbox  
+api.call('campaigns', 'template-content', { cid: 'd8e5d9c34d' }, function (error, data) {
+    if (error)
+        console.log(error.message);
+    else
+        //console.log(JSON.stringify(data));
+        console.log("successful campaign");
+});
+
+var MailChimpExportAPI = require('mailchimp').MailChimpExportAPI;
+ 
+try {
+    var exportApi = new MailChimpExportAPI(apiKey, { version : '1.0', secure: false });
+} catch (error) {
+    console.log(error.message);
+}
+ 
+exportApi.list({ id : 'd8e5d9c34d'  }, function (error, data) {
+    if (error)
+        console.log(error.message);
+    else
+        //console.log(JSON.stringify(data));
+        console.log("Successful Export");
+});
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'html')
