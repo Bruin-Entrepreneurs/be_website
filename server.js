14307e5857565b516b185ebbfde184465ccf6e8b
OmegaNalphA <agg_anshul@yahoo.com>
2016-09-05 22:43:07 -0700

    Finished integration, need styling
    Two different ways to do integration are detailed, one is through the
    html side and one is through server side. Can go either way. Needs
    styling from front end devs.

--- server.js
+++ server.js
@@ -19,23 +19,23 @@ try {
     console.log(error.message);
 }
 
+
+
 api.call('campaigns', 'list', { start: 0, limit: 25 }, function (error, data) {
     if (error)
         console.log(error.message);
     else
-        //console.log(JSON.stringify(data));
         console.log("successful access of lists"); 
 });
 
 
-//whats our campaign id? check the sandbox  
-api.call('campaigns', 'template-content', { cid: 'd8e5d9c34d' }, function (error, data) {
+api.call('campaigns', 'template-content', { cid: '2fa181053a' }, function (error, data) {
     if (error)
         console.log(error.message);
     else
-        //console.log(JSON.stringify(data));
         console.log("successful campaign");
 });
+
 
 var MailChimpExportAPI = require('mailchimp').MailChimpExportAPI;
  
@@ -49,9 +49,45 @@ exportApi.list({ id : 'd8e5d9c34d'  }, function (error, data) {
     if (error)
         console.log(error.message);
     else
-        //console.log(JSON.stringify(data));
         console.log("Successful Export");
 });
+
+/*
+var MailChimpOAuth = require('mailchimp').MailChimpOAuth;
+ 
+var options = {
+    clientId: 'Your MailChimp client id',
+    clientSecret: 'Your MailChimp client secret',
+    redirectUri: 'http://www.example.com',
+    ownServer: true,
+    addPort: true,
+    finalUri: 'http://www.example.com/successfulLogin.html'
+};
+ 
+var oauth = new MailChimpOAuth(options);
+ 
+console.log(oauth.getAuthorizeUri()); // The MailChimp login URI the user needs to be sent to 
+oauth.on('error', function (error) {
+    console.log(error.err);
+});
+ 
+oauth.on('authed', function (data) {
+ 
+    try {
+        var api = new MailChimpAPI(data.apiKey, { version : '1.3', secure : false });
+    } catch (error) {
+        console.log(error.message);
+    }
+ 
+    api.campaigns({ start: 0, limit: 25 }, function (error, data) {
+        if (error)
+            console.log(error.message);
+        else
+            console.log(JSON.stringify(data)); // Do something with your data! 
+    });
+ 
+});
+*/
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
