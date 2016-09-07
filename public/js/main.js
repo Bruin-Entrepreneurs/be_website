09f719d8a57e4f518856d0f1595509056b07212e
Hao Nguyen <haolnguyen7@gmail.com>
2016-09-07 03:25:25 -0700

    CHOW CHU

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(() => {
   console.log('time to get our shit together')
+
+  $(".navbar-toggle").click(function(event) {
+        $(".navbar-collapse").toggle('in');
+  });
+
 })
+
