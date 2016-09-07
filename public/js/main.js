db8f57530361e5a40c30f1eac689a9c0ada89a11
OmegaNalphA <agg_anshul@yahoo.com>
2016-09-07 01:20:19 -0700

    Validation

--- public/js/main.js
+++ public/js/main.js
@@ -20,7 +20,7 @@ function subscribe (email) {
     data: { email: email },
     error: (err) => {
       console.log('error', err)
-      alert("Subscribe unsuccessful. Please try again later.")
+      alert("Subscribe unsuccessful. Please try again.")
     },
     success: (data) => {
       alert("Congratulations! You have successfully subscribed. Check your email for the confirmation!")
@@ -29,7 +29,6 @@ function subscribe (email) {
 }
 
 function validateEmail (email) {
-  // TODO: Anshul -- finish email validation with link http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
   if (email){
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
